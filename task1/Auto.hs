7d3ae905700ea8086eb4f78e7aff1fbd82dcaaeb
Filip Czaplicki <filipczaplicki@gmail.com>
2017-03-15 11:09:35 +0100

    Task 1: non-obvious functions stubs.

--- task1/Auto.hs
+++ task1/Auto.hs
@@ -1,6 +1,7 @@
-module Auto ( ) where
+module Auto (Auto, accepts, emptyA, epsA, symA, leftA, sumA, thenA, fromLists, toLists) where
   data Auto a q = A { states :: [q], initStates  :: [q], isAccepting :: q -> Bool, transition  :: q -> a -> [q]}
-  -- accepts :: Eq q => Auto a q -> [a] -> Bool
+  accepts :: Eq q => Auto a q -> [a] -> Bool
+  accepts _ _ = True
   emptyA :: Auto a ()
   emptyA = A { states = [], initStates = [], isAccepting = const False, transition = \ _ _ -> []}
   epsA :: Auto a ()
@@ -8,7 +9,8 @@ module Auto ( ) where
   symA :: Eq a => a -> Auto a Bool
   symA x = A { states = [False, True], initStates = [False], isAccepting = id, transition = t} where
     t q a = [True | not q && a == x]
-  -- leftA :: Auto a q -> Auto a (Either q r)
+  leftA :: Auto a q -> Auto a (Either q r)
+  leftA _ = A { states = [], initStates = [], isAccepting = const False, transition = \ _ _ -> []}
   sumA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
   sumA aut1 aut2 = A { states = s, initStates = i, isAccepting = acc, transition = t} where
     s = map Left (states aut1) ++ map Right (states aut2)
@@ -17,6 +19,9 @@ module Auto ( ) where
                       Right q2 -> isAccepting aut2 q2
     t q a = case q of Left q1 -> map Left (transition aut1 q1 a)
                       Right q2 -> map Right (transition aut2 q2 a)
-  -- thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
-  -- fromLists :: (Eq q, Eq a) => [q] -> [q] -> [q] -> [(q,a,[q])] -> Auto a q
-  -- toLists :: (Enum a,Bounded a) => Auto a q -> ([q],[q],[q],[(q,a,[q])])
+  thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
+  thenA _ _ = A { states = [], initStates = [], isAccepting = const True, transition = \ _ _ -> []}
+  fromLists :: (Eq q, Eq a) => [q] -> [q] -> [q] -> [(q,a,[q])] -> Auto a q
+  fromLists _ _ _ _ = A { states = [], initStates = [], isAccepting = const False, transition = \ _ _ -> []}
+  toLists :: (Enum a,Bounded a) => Auto a q -> ([q],[q],[q],[(q,a,[q])])
+  toLists _ = ([], [], [], [])
