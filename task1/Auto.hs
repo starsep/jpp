c645cc3674e45a7a3d1c3957a11b6b0b22a9cd69
Filip Czaplicki <filipczaplicki@gmail.com>
2017-03-25 14:10:17 +0100

    Task 1: Auto: reformat (where removed).

--- task1/Auto.hs
+++ task1/Auto.hs
@@ -43,74 +43,63 @@ module Auto (Auto, accepts, emptyA, epsA, symA, leftA, sumA, thenA, fromLists, t
     states = [False, True],
     initStates = [False],
     isAccepting = id,
-    transition = t
-  } where
-    t q a = [True | not q && a == x]
+    transition = \ q a -> [True | not q && a == x]
+  }
 
   leftA :: Auto a q -> Auto a (Either q r)
   leftA aut = A {
     states = map Left (states aut),
     initStates = map Left (initStates aut),
-    isAccepting = isA,
-    transition = trans
-  } where
-    isA (Left x) = isAccepting aut x
-    isA (Right _) = False
-    trans (Left x) c = map Left (transition aut x c)
-    trans (Right _) _ = []
+    isAccepting = \ q -> case q of
+      Left x -> isAccepting aut x
+      Right _ -> False,
+    transition = \ q c -> case q of
+      Left x -> map Left (transition aut x c)
+      Right _ -> []
+  }
 
   sumA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
   sumA aut1 aut2 = A {
-    states = s,
-    initStates = i,
-    isAccepting = acc,
-    transition = t
-  } where
-    s = map Left (states aut1) ++ map Right (states aut2)
-    i = map Left (initStates aut1) ++ map Right (initStates aut2)
-    acc q = case q of
+    states = map Left (states aut1) ++ map Right (states aut2),
+    initStates = map Left (initStates aut1) ++ map Right (initStates aut2),
+    isAccepting = \ q -> case q of
       Left q1 -> isAccepting aut1 q1
-      Right q2 -> isAccepting aut2 q2
-    t q a = case q of
+      Right q2 -> isAccepting aut2 q2,
+    transition = \ q a -> case q of
       Left q1 -> map Left (transition aut1 q1 a)
       Right q2 -> map Right (transition aut2 q2 a)
+  }
 
   thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
   thenA aut1 aut2 = A {
-    states = s,
-    initStates = i,
-    isAccepting = acc,
-    transition = t
-  } where
-    s = map Left (states aut1) ++ map Right (states aut2)
-    i = map Left (initStates aut1) ++
-      map Right (
+    states = map Left (states aut1) ++ map Right (states aut2),
+    initStates = map Right (
         if acceptsEmpty aut1 then
           initStates aut2
-        else []
-      )
-    acc q = case q of
+          else []
+      ) ++ map Left (initStates aut1),
+    isAccepting = \ q -> case q of
       Left _ -> acceptsEmpty aut2
-      Right q2 -> isAccepting aut2 q2
-    t q a = case q of
+      Right q' -> isAccepting aut2 q',
+    transition = \ q a -> case q of
       Left q1 ->
         map Left (transition aut1 q1 a) ++
           if isAccepting aut1 q1 then
             map Right (initStates aut2)
           else []
       Right q2 -> map Right (transition aut2 q2 a)
+  }
 
   fromLists :: (Eq q, Eq a) => [q] -> [q] -> [q] -> [(q, a, [q])] -> Auto a q
   fromLists s i a t = A {
     states = s,
     initStates = i,
     isAccepting = (`elem` a),
-    transition = trans
-  } where
-    trans q c =
+    transition = \ q c ->
       foldr (\ (x, cc, l) acc ->
         if x == q && c == cc then l ++ acc else acc
       ) [] t
+  }
 
   toLists :: (Enum a, Bounded a) => Auto a q -> ([q], [q], [q], [(q, a, [q])])
   toLists aut =
