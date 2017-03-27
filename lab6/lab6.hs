63db91bf1a8540070d6ab9364e831f2525834332
Filip Czaplicki <filipczaplicki@gmail.com>
2017-03-27 16:59:01 +0200

    Lab6: Reader.

--- lab6/lab6.hs
+++ lab6/lab6.hs
@@ -1,20 +1,24 @@
+import Control.Monad.Reader
+
 data Help a = That { fun :: Int -> a }
 
 instance Monad Help where
   return a = That (const a)
   (>>=) (That f) update = That $ \ x -> (fun $ update $ f x) x
 
-ask :: Help Int
-ask = That id
+-- ask :: Help Int
+-- ask = That id
 
-local :: (Int -> Int) -> Help a -> Help a
-local upd (That f) = That $ f . upd
+-- local :: (Int -> Int) -> Help a -> Help a
+-- local upd (That f) = That $ f . upd
 
 data Tree a = Node (Tree a) a (Tree a) | Leaf
 
--- treeDepth :: Tree a -> Tree (Int, a)
+treeDepth :: Tree a -> Tree (Int, a)
+treeDepth t = treeDRec t 0
 
-treeDRec :: Tree a -> Help (Tree (Int, a))
+treeDRec :: Tree a -> Int -> Tree (Int, a)
+-- treeDRec :: Tree a -> Help (Tree (Int, a))
 treeDRec Leaf = return Leaf
 treeDRec (Node tl x tr) = do {
   d <- ask;
