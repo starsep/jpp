6fe55e1a19d9ccaebdb16014ca6826b20d84101d
Filip Czaplicki <filipczaplicki@gmail.com>
2017-03-27 16:52:33 +0200

    Lab6: init.

---
+++ lab6/lab6.hs
@@ -0,0 +1,24 @@
+data Help a = That { fun :: Int -> a }
+
+instance Monad Help where
+  return a = That (const a)
+  (>>=) (That f) update = That $ \ x -> (fun $ update $ f x) x
+
+ask :: Help Int
+ask = That id
+
+local :: (Int -> Int) -> Help a -> Help a
+local upd (That f) = That $ f . upd
+
+data Tree a = Node (Tree a) a (Tree a) | Leaf
+
+-- treeDepth :: Tree a -> Tree (Int, a)
+
+treeDRec :: Tree a -> Help (Tree (Int, a))
+treeDRec Leaf = return Leaf
+treeDRec (Node tl x tr) = do {
+  d <- ask;
+  ntl <- local (+1) (treeDRec tl);
+  ntr <- local (+1) (treeDRec tr);
+  return (Node ntl (d, x) ntr)
+}
