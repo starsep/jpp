cb05e9f0a7c38495f0e4275ae700d0c17ccc1389
Filip Czaplicki <filipczaplicki@gmail.com>
2017-05-18 13:13:58 +0200

    Task 1 -> task_haskell, task_prolog, lab_prolog/if.pl added.

--- lab_prolog/quicksort.pl
+++ lab_prolog/quicksort.pl
@@ -4,9 +4,16 @@ split([A | X], E, [A | L], R) :- A < E, split(X, E, L, R).
 split([A | X], E, L, [A | R]) :- A >= E, split(X, E, L, R).
 
 qsort([], []).
-qsort([E | X], Z) :- split(X, E, L, R), qsort(L, L2), qsort(R, R2), append(L2, [E | R2], Z).
+qsort([E | X], Z) :-
+  split(X, E, L, R),
+  qsort(L, L2),
+  qsort(R, R2),
+  append(L2, [E | R2], Z).
 
 qsort_acc([], A, A).
-qsort_acc([E | X], A, U) :- split(X, E, L, R), qsort_acc(R, A, R2), qsort_acc(L, [E | R2], U).
+qsort_acc([E | X], A, U) :-
+  split(X, E, L, R),
+  qsort_acc(R, A, R2),
+  qsort_acc(L, [E | R2], U).
 
 qsort2(X, Y) :- qsort_acc(X, [], Y).
