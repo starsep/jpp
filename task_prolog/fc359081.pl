106c4757adc43fc81a01e6693641237ff4401933
Filip Czaplicki <filipczaplicki@gmail.com>
2017-06-02 23:08:35 +0200

    Task prolog: closure (currently looping).

--- task_prolog/fc359081.pl
+++ task_prolog/fc359081.pl
@@ -2,24 +2,89 @@
 
 :- use_module(library(lists)).
 
-% createLR(+Grammar, -Automat, -Info)
+% createLR(+Grammar, -Automaton, -Info)
 createLR(G, A, I) :-
   extendGrammar(G, E),
-  createAutomat(E, A, I).
+  createAutomaton(E, A, I).
 
-% createAutomat(+Grammar, -Automat, -Info)
-createAutomat(G, A, I) :-
-  I = yes,
+% createAutomaton(+Grammar, -Automaton, -Info)
+createAutomaton(G, A, I) :-
+  buildTable(G, T, InfoTable),
+  ( InfoTable \== yes ->
+    makeConflict(InfoTable, A, I)
+  ;
+    createAutomatonWithTable(G, T, A, I)
+  ).
+
+% buildTable(+Grammar, -Table, -Info)
+buildTable(G, T, I) :-
+  G = gramatyka(S, L),
+  clojure([item('Z', [S, #], 0)], L, InitState),
+  write(InitState),
+  T = null,
+  I = yes.
+
+% createAutomatonWithTable(+Grammar, +Table, -Automaton, -Info)
+createAutomatonWithTable(G, T, A, I) :-
+  A = null,
+  I = yes.
+
+% makeConflict(+ErrorMessage, -Automaton, -Info)
+makeConflict(E, A, I) :-
+  I = konflikt(E),
   A = null.
 
-% accept(+Automat, +Word)
-accept(A, W).
+% clojure(+Items, +ProductionsList, -ClojureOfItems)
+clojure(I, P, C) :-
+  clojureOnce(I, P, C1),
+  clojureOnce(C1, P, C2),
+  ( C1 == C2 -> C = C2 ; clojure(C2, P, C) ).
+
+% clojureOnce(+Items, +ProductionsList, -ClojureOfItems)
+clojureOnce([], _, C) :-
+  C = [].
+
+clojureOnce([H | T], P, C) :-
+  clojureItem(H, P, CH),
+  clojureOnce(T, P, CT),
+  append(CH, CT, C).
+
+% clojureItem(+Item, +ProductionsList, -ClojureOfItem)
+clojureItem(I, P, C) :-
+  I = item(_, R, Index),
+  length(R, Len),
+  ( Len =< Index -> C = [I] ;
+    nth0(Index, R, L),
+    itemsFromProductions(L, P, NewItems),
+    append([I], NewItems, C),
+    write(C)
+  ).
+
+% itemsFromProductions(+Symbol, +ProductionsList, -Items)
+itemsFromProductions(_, [], I) :- I = [].
+itemsFromProductions(S, [H | T], I) :-
+  H = prod(L, R),
+  ( L = S ->
+    rightSidesToItems(L, R, I)
+  ;
+    itemsFromProductions(S, T, I)
+  ).
+
+% rightSidesToItems(+LeftSymbol, +RightSidesList, -Items)
+rightSidesToItems(_, [], I) :- I = [].
+rightSidesToItems(L, [H | T], I) :-
+  rightSidesToItems(L, T, IT),
+  Item = item(L, H, 0),
+  I = [Item | IT].
+
+
+% accept(+Automaton, +Word)
+accept(_A, _W).
 
 % extendGrammar(+Grammar, -ExtendedGrammar)
 extendGrammar(G, E) :-
   G = gramatyka(S, L),
-  E = gramatyka('Z', [prod('Z', [[nt(S), #]]) | L]),
-  debugGrammar(E).
+  E = gramatyka(S, [prod('Z', [[nt(S), #]]) | L]).
 
 % --------------------------- DEBUG -------------------------------------
 
--- task_prolog/tests.pl
+++ task_prolog/tests.pl
@@ -2,10 +2,29 @@
 :- initialization(main).
 main :-
   consult(fc359081),
-  test(ex1, [[id], ['(',id,')'], [id,'+',ident], [id,'+',id]]),
+  test(ex0, []),
+  % test(ex1, [[id], ['(',id,')'], [id,'+',ident], [id,'+',id]]),
+  % test(ex2, [[id], ['(',id,')'], [id,'+',ident], [id,'+',id]]),
+  % test(ex3, [[id], ['(',id,')'], [id,'+',ident], [id,'+',id]]),
+  % test(ex4, [[id], ['(',id,')'], [id,'+',ident], [id,'+',id]]),
+  % test(ex5, [[id], ['(',id,')'], [id,'+',ident], [id,'+',id]]),
+  % test(ex6, [[id], ['(',id,')'], [id,'+',ident], [id,'+',id]]),
   halt.
 main :-
   halt(1).
+
+% LR(0)
+grammar(ex0, gramatyka('E', [
+  prod('E', [
+    [nt('E'), '*', nt('B')],
+    [nt('E'), '+', nt('B')],
+    [nt('B')]
+  ]),
+  prod('B', [
+    ['0'],
+    ['1']
+  ])
+])).
 
 % LR(0)
 grammar(ex1, gramatyka('E', [
@@ -20,24 +39,67 @@ grammar(ex1, gramatyka('E', [
 ])).
 
 % LR(0)
-grammar(ex2, gramatyka('A', [prod('A', [[nt('A'), x], [x]])])).
+grammar(ex2, gramatyka('A', [
+  prod('A', [
+    [nt('A'), x],
+    [x]
+  ])
+])).
 
 % SLR(1)
-grammar(ex3, gramatyka('A', [prod('A', [[x, nt('A')], [x]])])).
+grammar(ex3, gramatyka('A', [
+  prod('A', [
+    [x, nt('A')],
+    [x]
+  ])
+])).
 
 % nie SLR(1)
-grammar(ex4, gramatyka('A', [prod('A', [[x, nt('B')], [nt('B'), y], []]), prod('B', [[]])])).
+grammar(ex4, gramatyka('A', [
+  prod('A', [
+    [x, nt('B')],
+    [nt('B'), y],
+    []
+  ]),
+  prod('B', [
+    []
+  ])
+])).
 
 % nie SLR(1)
-grammar(ex5, gramatyka('S', [prod('S', [[id], [nt('V'), ':=', nt('E')]]), prod('V', [[id], [id, '[', nt('E'), ']']]), prod('E', [[v]])])).
+grammar(ex5, gramatyka('S', [
+  prod('S', [
+    [id],
+    [nt('V'), ':=', nt('E')]
+  ]),
+  prod('V', [
+    [id],
+    [id, '[', nt('E'), ']']
+  ]),
+  prod('E', [
+    [v]
+  ])
+])).
 
 % nie SLR(1)
-grammar(ex6, gramatyka('A', [prod('A', [[x], [nt('B'), nt('B')]]), prod('B', [[x]])])).
+grammar(ex6, gramatyka('A', [
+  prod('A', [
+    [x],
+    [nt('B'), nt('B')]
+  ]),
+  prod('B', [
+    [x]
+  ])
+])).
 
 % test(+NazwaGramatyki, +ListaSlowDoZbadania)
 test(NG, ListaSlow) :-
   grammar(NG, G),
-  createLR(G, Automat, yes),
+  debugGrammar(G),
+  createLR(G, Automat, Info),
+  write('Automat building info: '),
+  write(Info),
+  write('\n'),
   checkWords(ListaSlow, Automat).
 
 checkWords([], _) :-
